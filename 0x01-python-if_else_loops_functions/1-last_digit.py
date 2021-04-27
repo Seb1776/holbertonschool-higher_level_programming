f3057704b752fd3eea2755c31593a07c17ad7287
Seb1776 <sebastianwho1776@gmail.com>
2021-04-27 22:24:59 +0000

    Fixed the recent deleted variable interactions with conditionals

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 
-if last > 5:
+if number % 10 > 5:
     print('Last digit of {:d} is {:d} and is greater than 5'.format(
         number, number % 10))
 
-if last == 0:
+if number % 10 == 0:
     print('Last digit of {:d} is {:d} and is 0'.format(
         number, number % 10))
 
-if last < 6 and last is not 0:
+if number % 10 < 6 and number % 10 is not 0:
     print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(
         number, number % 10))
