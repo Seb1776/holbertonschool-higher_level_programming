b9c2ea9986e426073416e6cb0b75f1405b3f211a
Seb1776 <sebastianwho1776@gmail.com>
2021-04-28 04:54:31 +0000

    Updated code to work with the new parameter name

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def islower( c ):
-    if (ord(char) >= 97 and ord(char) <= 122):
+    if (ord(c) >= 97 and ord(c) <= 122):
         return True
     else:
         return False
