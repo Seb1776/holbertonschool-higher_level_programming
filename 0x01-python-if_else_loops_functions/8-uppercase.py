4c31c8f560a23160b763786eb47bbc018cd91b38
Seb1776 <sebastianwho1776@gmail.com>
2021-04-28 04:51:27 +0000

    Added point 8, 8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    for chars in range(0, len(str)):
+        if (ord(str[chars]) >= 97 and ord(str[chars]) <= 122):
+            x = 32
+        else:
+            x = 0
+
+        y = ord(str[chars]) - x
+        print('{}'.format(chr(y)), end='')
+
+    print('')
