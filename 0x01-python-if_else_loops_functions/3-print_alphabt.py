3ce7a40e627a70ee8b3c07ff650ea6626799188d
Seb1776 <sebastianwho1776@gmail.com>
2021-04-28 04:03:41 +0000

    Updated 3-print_alphabt code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for letter_code in range(ord('a'), ord('z')+1):
-    letter = chr(letter_code)
-    if letter not in "qe":
-        print(letter, end="")
+    if letter_code != 101 and letter_code != 113:
+        print("{}".format(chr(letter_code)), end="")
