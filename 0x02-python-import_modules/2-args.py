74fabda74907e109a90572dcdaac4fd10b17d543
Seb1776 <sebastianwho1776@gmail.com>
2021-04-28 22:13:53 +0000

    Fixed specifics

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,11 @@ import sys
 if (len(sys.argv) - 1) == 0:
     print('0 arguments.')
 else:
-    print('{:d} arguments:'.format(len(sys.argv) - 1))
+    if (len(sys.argv) - 1) == 1:
+        print('{:d} argument:'.format(len(sys.argv) - 1))
+    else:
+        print('{:d} arguments:'.format(len(sys.argv) - 1))
+
     for x in range(len(sys.argv)):
         if x != 0:
             print('{:d}: {}'.format(x, str(sys.argv[x])))
