41cde422cc33f562634e465ea2c55b65f777bf71
Seb1776 <sebastianwho1776@gmail.com>
2021-04-28 22:10:51 +0000

    Fixed file name import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,22 +2,10 @@
 
 
 if __name__ == "__main__":
-    from calculator_1 import add
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-
-    from calculator1 import sub
-    a = 10
-    b = 5
     print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-
-    from calculator1 import mul
-    a = 10
-    b = 5
     print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-
-    from calculator1 import div
-    a = 10
-    b = 5
     print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
