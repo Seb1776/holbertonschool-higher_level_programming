0bdd706bba23c550663fc2f8fa875cd490f0b336
Seb1776 <sebastianwho1776@gmail.com>
2021-04-28 16:08:57 +0000

    Fixed final number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 
 for x in range(0, 10):
     for y in range(x + 1, 10):
-        print('{:d}{:d}, '.format(x, y), end='')
-
-print('89')
+        if x == 8 and y == 9:
+            print('{:d}{:d}'.format(x, y))
+        else:
+            print('{:d}{:d}, '.format(x, y), end='')
