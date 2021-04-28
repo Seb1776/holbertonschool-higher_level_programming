0aa8d9b85a06b59fa4b2af024815752b5c8f9837
Seb1776 <sebastianwho1776@gmail.com>
2021-04-28 04:01:22 +0000

    Updated 2-print_alphabet code

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for alpha_letters in range(97, 122):
-    print("{:c}".format(alpha_letters), end="")
+for alpha_letters in range(97, 123):
+    print("{}".format(chr(alpha_letters)), end="")
