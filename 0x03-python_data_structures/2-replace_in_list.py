4c02dc3c3ede26bceaca6fb3c5e04f0de87732d3
Seb1776 <sebastianwho1776@gmail.com>
2021-05-05 14:33:41 +0000

    Added point 2, 2-replace_in_list

---
+++ 0x03-python_data_structures/2-replace_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    listlen = len(my_list) - 1
+
+    if idx > listlen:
+        return my_list
+
+    elif idx < 0:
+         return my_list
+
+    else:    
+        my_list[idx] = element
+        return my_list
