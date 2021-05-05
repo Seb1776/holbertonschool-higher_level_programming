45482cd98de9f0f119d7830b05b490e152676c28
Seb1776 <sebastianwho1776@gmail.com>
2021-05-05 19:47:37 +0000

    Fixed PEP8 style error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,8 +8,8 @@ def replace_in_list(my_list, idx, element):
         return my_list
 
     elif idx < 0:
-         return my_list
+        return my_list
 
-    else:    
+    else:
         my_list[idx] = element
         return my_list
