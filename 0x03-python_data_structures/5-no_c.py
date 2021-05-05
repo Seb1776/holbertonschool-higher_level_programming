f48d41ff0af8a7e483d0a5c4cda9aa7c0e410c67
Seb1776 <sebastianwho1776@gmail.com>
2021-05-05 15:30:41 +0000

    Added point 5, 5-no_c

---
+++ 0x03-python_data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    for c in my_string:
+        if c != None:
+            current_c = ord(c)
+
+            if current_c == 67 or current_c == 99:
+                c = ''
+
+            print(c, end='')
