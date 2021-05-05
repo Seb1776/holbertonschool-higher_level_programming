08260c0157961d1307cfe85de1ba8b09d3e9dbd1
Seb1776 <70917701+Seb1776@users.noreply.github.com>
2021-05-05 16:50:54 -0500

    Fixed empty list conditional

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,9 @@
 
 def print_reversed_list_integer(my_list=[]):
     listlen = len(my_list) - 1
-
-    for i in range(listlen, -1, -1):
-        print('{:d}'.format(my_list[i]))
+    
+    if listlen < 0:
+        return None
+    else:
+        for i in range(listlen, -1, -1):
+            print('{:d}'.format(my_list[i]))
