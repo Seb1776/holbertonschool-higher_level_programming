07832c0e1e6a2b2aa568fea92b1a05b613b9f326
Seb1776 <sebastianwho1776@gmail.com>
2021-05-05 16:11:27 +0000

    Added point 6, 6-print_matrix_integer

---
+++ 0x03-python_data_structures/6-print_matrix_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    i = 0
+
+    if matrix is not None:
+        for mx in matrix:
+            if i == 3:
+                print('')
+                i = 0
+
+            for my in mx:
+                i += 1
+
+                if i != 3:
+                    print('{}'.format(my), end=' ')
+                else:
+                    print('{}'.format(my), end='')
+        
+        print('')
