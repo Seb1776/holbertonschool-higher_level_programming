ff02e0466ba10c58cf7b23715a8711ca414e3f61
Seb1776 <sebastianwho1776@gmail.com>
2021-05-05 17:08:05 +0000

    Added point 7, 7-add_tuple

---
+++ 0x03-python_data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_tup_a = len(tuple_a)
+    len_tup_b = len(tuple_b)
+
+    if len_tup_a >= 2 and len_tup_b >= 2:
+        res_tup = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    else:
+        if len_tup_a == 1:
+            res_tup = ((tuple_a[0] + tuple_b[0]), (0 + tuple_b[1]))
+        elif len_tup_b == 1:
+            res_tup = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0))
+        elif len_tup_a == 0:
+            res_tup = ((0 + tuple_b[0]), (0 + tuple_b[1]))
+        elif len_tup_b == 0:
+            res_tup = ((tuple_a[0] + 0), (tuple_a[1] + 0))
+    
+    return res_tup
