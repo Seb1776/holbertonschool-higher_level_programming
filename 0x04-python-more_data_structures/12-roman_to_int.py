cb280c912c9df09c7e9a815591e47071719f929b
Seb1776 <sebastianwho1776@gmail.com>
2021-05-06 23:36:27 +0000

    Fixed conditional to check for empty strings or a non string variable

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,26 +2,29 @@
 
 
 def roman_to_int(roman_string):
-    romans = {
-        'I': 1, 'V': 5,
-        'X': 10, 'L': 50,
-        'C': 100, 'D': 500,
-        'M': 1000
-    }
+    if isinstance(roman_string, str) or roman_string is not None:   
+        romans = {
+            'I': 1, 'V': 5,
+            'X': 10, 'L': 50,
+            'C': 100, 'D': 500,
+            'M': 1000
+        }
 
-    roman_backward = list(reversed(list(roman_string)))
-    val = 0
+        roman_backward = list(reversed(list(roman_string)))
+        val = 0
 
-    right = romans[roman_backward[0]]
+        right = romans[roman_backward[0]]
 
-    for n in roman_backward:
-        left = romans[n]
+        for n in roman_backward:
+            left = romans[n]
 
-        if left < right:
-            val -= left
-        else:
-            val += left
+            if left < right:
+                val -= left
+            else:
+                val += left
 
-        right = left
+            right = left
 
-    return val
+        return val
+    else:
+        return 0
