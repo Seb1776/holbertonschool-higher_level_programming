af9679eaa318a892c720d309d90d3cbef3a9cc66
Seb1776 <sebastianwho1776@gmail.com>
2021-05-06 15:28:23 +0000

    Added extra conditional for lenght 0 tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,6 +9,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         res_tup = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     elif len_tup_a == 1 and len_tup_b == 1:
         res_tup = ((tuple_a[0] + 0), (tuple_b[0] + 0))
+    elif len_tup_a <= 0 and len_tup_b <= 0:
+        res_tup = (0, 0)
     else:
         if len_tup_a == 1:
             res_tup = ((tuple_a[0] + tuple_b[0]), (0 + tuple_b[1]))
