8d24a74c40997751f48f1eaefecb9610840a1eeb
Seb1776 <sebastianwho1776@gmail.com>
2021-05-06 23:19:27 +0000

    Added point 12, 12-roman_to_int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    romans = {
+        'I': 1, 'V': 5,
+        'X': 10, 'L': 50,
+        'C': 100, 'D': 500,
+        'M': 1000
+    }
+
+    roman_backward = list(reversed(list(roman_string)))
+    val = 0
+
+    right = romans[roman_backward[0]]
+
+    for n in roman_backward:
+        left = romans[n]
+
+        if left < right:
+            val -= left
+        else:
+            val += left
+
+        right = left
+
+    return val
