9ec16f93a0ef7d033531a6526d9c4071178539f8
Seb1776 <sebastianwho1776@gmail.com>
2021-05-06 02:43:03 +0000

    All code rewritten

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,12 +2,10 @@
 
 
 def no_c(my_string):
-    lst = list(my_string)
-    i = 0
+    res = ""
 
-    for c in lst:
-        if c is 'c' or c is 'C':
-            lst[i] = ''
-        i += 1
-
-    return "".join(lst)
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            res += i
+    
+    return res
