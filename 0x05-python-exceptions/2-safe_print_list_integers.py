79a20ac7c37606f355b1e6cfcce7e74226bff4e8
Seb1776 <sebastianwho1776@gmail.com>
2021-05-18 01:46:26 +0000

    Added point 2, 2-safe_print_list_integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    round = 0
+
+    for y in range(x):
+        try:
+            print('{:d}'.format(my_list[y]), end='')
+            round += 1
+
+        except (ValueError, TypeError):
+            continue
+    
+    print()
+    return round
