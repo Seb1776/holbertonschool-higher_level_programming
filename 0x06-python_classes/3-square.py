de22f85bb4bad22bb1db59700d62a608a3294d15
Seb1776 <sebastianwho1776@gmail.com>
2021-05-19 04:39:24 +0000

    Added point 3, 3-square

---
+++ 0x06-python_classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Class Square'''
+
+
+class Square:
+    '''Class Square'''
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+
+        if self.__size < 0:
+            raise ValueError('sizer must be >= 0')
+
+    def area(self):
+        return self.__size ** 2
