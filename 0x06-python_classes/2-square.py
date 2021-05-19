4d42f272c27c1c201e2afd67c31f16746109463b
Seb1776 <sebastianwho1776@gmail.com>
2021-05-19 04:33:40 +0000

    Added point 2, 2-square

---
+++ 0x06-python_classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Class Square'''
+
+
+class Square:
+    '''Class Square'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    if not isinstance(self.__size, int):
+        raise TypeError('size must be an integer')
+
+    if self.__size < 0:
+        raise ValueError('sizer must be >= 0')
