aee684431e2dbf2b2f2b80d4d0c8681a463487dd
Seb1776 <sebastianwho1776@gmail.com>
2021-05-19 04:47:30 +0000

    Added point 4, 4-square

---
+++ 0x06-python_classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''Class Square'''
+
+
+class Square:
+    '''Class Square'''
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+
+        if self.__size < 0:
+            raise ValueError('sizer must be >= 0')
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+        elif self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        return self.__size ** 2
