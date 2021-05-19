6ae76c7344b07c7910f646cf57247ae980f8dc0a
Seb1776 <sebastianwho1776@gmail.com>
2021-05-19 04:38:56 +0000

    Fixed self not being recognized

--- 0x06-python_classes/2-square.py
+++ 0x06-python_classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-    if not isinstance(self.__size, int):
-        raise TypeError('size must be an integer')
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
 
-    if self.__size < 0:
-        raise ValueError('sizer must be >= 0')
+        if self.__size < 0:
+            raise ValueError('sizer must be >= 0')
