4a12df678544b9ed8d36fea2f47ac3180797947b
Seb1776 <sebastianwho1776@gmail.com>
2021-05-19 04:57:39 +0000

    Added point 6, 6-square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''Class Square'''
+
+
+class Square:
+    '''Class Square'''
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+        elif self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def position(self):
+        return self.__position    
+
+    @position.setter
+    def position(self, value):
+        if (type(value) != tuple or len(value) is not 2 or 
+                type(value[0]) is not int or value[0] < 0 or
+                type(value[1]) is not int or value[1] < 0):
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size <= 0:
+            print()
+        else:
+            for x in range(self.__size):
+                print("{}".format("#" * self.__size))
