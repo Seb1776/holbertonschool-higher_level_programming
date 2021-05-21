1cf76f559b3966d2d1d3c308b0488d55e51db77a
Seb1776 <sebastianwho1776@gmail.com>
2021-05-21 02:40:28 +0000

    Fixed return values

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,7 +47,13 @@ class Rectangle:
             raise ValueError('height must be >= 0')
 
     def perimeter(self):
-        return (self.__height + self.__width) * 2
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        else:
+            return (self.__height + self.__width) * 2
 
     def area(self):
-        return self.__width * self.__height
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        else:
+            return self.__width * self.__height
