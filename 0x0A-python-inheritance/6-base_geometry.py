cb0e9371e862cc0feee933ef386cd6bb8681b155
Seb1776 <sebastianwho1776@gmail.com>
2021-06-02 04:17:44 +0000

    Added point 6, 6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Description'''
+
+
+class BaseGeometry:
+    '''
+    Gemoetry Class
+
+    Methods:
+
+    area() -- Empty for now
+    '''
+
+    def area():
+        '''Empty area method'''
+        raise Exception('area() is not implemented')
