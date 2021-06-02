4c47f07965ce356858935ec2c2a6432b069514e6
Seb1776 <sebastianwho1776@gmail.com>
2021-06-02 03:43:50 +0000

    Added point 1, 1-my_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Description'''
+
+
+class MyList(list):
+    '''Class To Print A Sorted List'''
+
+    def print_sorted(self):
+        '''Sorting list process'''
+        print(sorted(self))
