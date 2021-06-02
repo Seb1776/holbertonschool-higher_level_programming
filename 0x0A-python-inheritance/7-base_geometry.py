135d5dfe61fac23837b08cf20bbeaf0bdc5da485
Seb1776 <sebastianwho1776@gmail.com>
2021-06-02 04:28:54 +0000

    Added point 7, 7-base_geometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Description'''
+
+
+class BaseGeometry:
+    '''
+    Gemoetry Class
+
+    Methods:
+
+    area() -- Empty for now
+
+    integer_validator(self, name, value) -- Validates a given integer
+        with a given string [name]
+
+        Args => self: Itself
+                name: Name of the value
+                value: Value of the data
+    '''
+
+    def area():
+        '''Empty area method'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''Validates an integer with a given name'''
+
+        if type(value) != int:
+            raise TypeError('{} must be integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
