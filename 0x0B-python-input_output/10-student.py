bb55512a21e758bdfc0386833b81d9a490904b87
Seb1776 <sebastianwho1776@gmail.com>
2021-06-04 03:44:17 +0000

    Added point 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''JSON Serialization'''
+
+
+class Student:
+    '''Student Class'''
+
+    def __init__(self, first_name, last_name, age):
+        '''Class definition'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Return a representation of the dictionary'''
+        if attrs is None:
+            return self.__dict__
+
+        new = {}
+
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new[key] = value
+
+        return new
