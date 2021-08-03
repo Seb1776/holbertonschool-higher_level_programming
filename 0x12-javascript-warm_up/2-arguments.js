e2dbaf98307c38202675474136eb8d6c61aaa6ad
Seb1776 <sebastianwho1776@gmail.com>
2021-08-03 04:54:28 +0000

    Added 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined)
+{
   console.log('No argument');
+}
 
 else if (process.argv[2] !== undefined && process.argv[3] === undefined)
+{
   console.log('Argument found');
+}
 
-else 
+else
+{
   console.log('Arguments found');
+}
