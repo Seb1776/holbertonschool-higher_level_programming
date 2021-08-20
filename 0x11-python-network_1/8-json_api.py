7f7386192ef79b1532747d5e5c823454c410fe97
Seb1776 <sebastianwho1776@gmail.com>
2021-08-20 22:23:51 +0000

    Added point 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""JSON API"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    """JSON API"""
+
+    url = "http://0.0.0.0:5000/search_user"
+    
+    if len(argv) > 1:
+        data = {"q": argv[1]}
+    
+    else:
+        data = {"q": ""}
+    
+    req = requests.post(url, data=data)
+
+    try:
+        json_response = req.json()
+
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
