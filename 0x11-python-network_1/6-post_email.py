fe16c6c5e674b7fff0e35da8d99cd558b4086bce
Seb1776 <sebastianwho1776@gmail.com>
2021-08-20 22:07:32 +0000

    Added point 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Post Email"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    """Post Email"""
+
+    url = argv[1]
+    mail = {"email", argv[2]}
+
+    req = requests.post(url, mail)
+    print(req.text)
