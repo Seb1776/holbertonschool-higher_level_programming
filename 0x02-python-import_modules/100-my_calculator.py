245181b36035a5d029c674cd2afa2807c9a87725
Seb1776 <sebastianwho1776@gmail.com>
2021-11-05 18:16:20 +0000

    Fixed 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calcutator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
     from sys import argv
 
     arglen = len(argv) - 1
