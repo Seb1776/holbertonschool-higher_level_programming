acf8c9c4410c4008c820afa4a58c7c4ad3f30ccc
Seb1776 <sebastianwho1776@gmail.com>
2021-11-05 18:10:17 +0000

    Added advc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calcutator_1 import add, sub, mul, div
+    from sys import argv
+
+    arglen = len(argv) - 1
+    ops = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+
+    if not arglen == 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[2] not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        res = ops[argv[2]](int(argv[1]), int(argv[3]))
+        print("{:d} {} {:d} = {:d}".
+              format(int(argv[1]), argv[2], int(argv[3]), res))
